1a78637a80036893d589e9275bdc268ce4daa48c
Pooja s mohan <poojasmohan04@gmail.com>
2025-08-12 07:54:20 +0530

    package

---
+++ package/Demo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo {
+   
+    public String info = "Learning Java packages is easy and fun!";
+
+
+    public void printInfo() {
+        System.out.println("You are running code from the 'demo' package.");
+    }
+}
---
+++ package/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import demo.Demo;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Demo demoInstance = new Demo();
+
+        
+        System.out.println(demoInstance.info);
+
+   
+        demoInstance.printInfo();
+    }
+}
